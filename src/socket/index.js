f231753f517672fe1dd1c9ae3a29253d5b1c665f
Arthur Diniz <arthurvdinizs@gmail.com>
2019-08-08 15:20:13 -0300

    socket queue test

--- src/socket/index.js
+++ src/socket/index.js
@@ -5,21 +5,22 @@ const { CheckIn } = require('../models/checkIn');
 
 io.on('connection', socket => {
   socket.on('queue', data => {
-    const checkIn = new CheckIn({
-      user: data.user,
-      place: {
-        ...data.place
-      }
-    });
-    checkIn.save(() => {
-      socket.broadcast.emit('queue', data);
-      // const job = new cron.CronJob('10 * * * * *', () => {
-      //   const d = new Date();
-      //   console.log('At One Minute:', d);
-      //   data.queue = data.queue - 1;
-      //   io.emit('queue', data);
-      //   job.stop();
-      // }, null, true);
-    })
+    socket.broadcast.emit('queue', data);
+    // const checkIn = new CheckIn({
+    //   user: data.user,
+    //   place: {
+    //     ...data.place
+    //   }
+    // });
+    // checkIn.save(() => {
+    //   socket.broadcast.emit('queue', data);
+    //   // const job = new cron.CronJob('10 * * * * *', () => {
+    //   //   const d = new Date();
+    //   //   console.log('At One Minute:', d);
+    //   //   data.queue = data.queue - 1;
+    //   //   io.emit('queue', data);
+    //   //   job.stop();
+    //   // }, null, true);
+    // })
   });
 });
