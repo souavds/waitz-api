9d473b4b4b2e325df2c12c256e4604da7f73653e
Arthur Diniz <arthurvdinizs@gmail.com>
2019-08-11 20:36:42 -0300

    narbyPlaces from db

--- src/controllers/PlaceController.js
+++ src/controllers/PlaceController.js
@@ -2,49 +2,49 @@ const gmaps = require('../config/gmaps');
 const Models = require('../models');
 
 const getNearbyPlaces = async (req, res) => {
-  const gmapsRes = await gmaps.placesNearby({
-    location: [req.query.lat, req.query.lng],
-    radius: 50000,
-    type: 'hospital'
-  }).asPromise()
-  .then((response) => {
-    return response.json.results
-  })
-  .catch((err) => {
-    return err;
-  });
+  // const gmapsRes = await gmaps.placesNearby({
+  //   location: [req.query.lat, req.query.lng],
+  //   radius: 50000,
+  //   type: 'hospital'
+  // }).asPromise()
+  // .then((response) => {
+  //   return response.json.results
+  // })
+  // .catch((err) => {
+  //   return err;
+  // });
 
-  const gmaps_ids = [];
-  gmapsRes.map(place => {
-    gmaps_ids.push(place.id);
-  });
+  // const gmaps_ids = [];
+  // gmapsRes.map(place => {
+  //   gmaps_ids.push(place.id);
+  // });
 
-  const placesInDB = await Models.Place.find({ gmaps_id: { $in: gmaps_ids } });
+  // const placesInDB = await Models.Place.find({ gmaps_id: { $in: gmaps_ids } });
   
-  const placesNotInDB = [];
-  const toSentPlaces = [...placesInDB];
-  if (placesInDB.length === 0) {
-    gmapsRes.map(place => {
-      const newPlace = {
-        gmaps_id: place.id,
-        reference: place.reference,
-        name: place.name,
-        geometry: {...place.geometry},
-        rating: place.rating,
-        vicinity: place.vicinity,
-      }
-      placesNotInDB.push(newPlace);
-    })
-    await Models.Place.create(placesNotInDB, (err, array) => {
-      if (array) {
-        toSentPlaces.push(...array);
-      }
-    });
-  }
+  // const placesNotInDB = [];
+  // const toSentPlaces = [...placesInDB];
+  // if (placesInDB.length === 0) {
+  //   gmapsRes.map(place => {
+  //     const newPlace = {
+  //       gmaps_id: place.id,
+  //       reference: place.reference,
+  //       name: place.name,
+  //       geometry: {...place.geometry},
+  //       rating: place.rating,
+  //       vicinity: place.vicinity,
+  //     }
+  //     placesNotInDB.push(newPlace);
+  //   })
+  //   await Models.Place.create(placesNotInDB, (err, array) => {
+  //     if (array) {
+  //       toSentPlaces.push(...array);
+  //     }
+  //   });
+  // }
 
-  return res.json(toSentPlaces);
-  // const places = await Models.Place.find();
-  // return res.json(places);
+  // return res.json(toSentPlaces);
+  const places = await Models.Place.find();
+  return res.json(places);
 }
 
 module.exports = {
